9b3bb265a0441f33e502305871e6cb430dc65882
UchidaYuma <alberorana@gmail.com>
2019-12-11 16:12:06 +0900

    first-commit

---
+++ src/Api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export function fetch(category = null) {
+  return axios.get('https://newsapi.org/v2/top-headlines', {
+    params: {
+      // ここにクエリパラメータを指定する
+      'country': 'jp',
+      'apiKey': process.env.REACT_APP_NEWS_API_KEY,
+      'category': category,
+    }
+  }).then((results) => {
+    console.log(results.data.articles);
+    return results.data.articles;
+  })
+    .catch((error) => {
+      console.log(error);
+      return 'ニュースの取得に失敗しました。';
+    })
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col, Card } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+import * as Api from "./Api.js";
+
+let newsPanel
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      news: []
+    };
+  }
+
+  async componentDidMount() {
+    let news = await Api.fetch();
+    console.log(news);
+    this.setState({ 'news': news })
+  }
+
+  render() {
+    console.log(this.state.news)
+    const newsPanel = this.state.news.map(val =>
+      <Col md="4" key={val.id}>
+        <a href={val.url}>
+          <Card className='card-body'>
+            <Card.Img className='card-image' variant="top" src={val.urlToImage} />
+            <Card.Body>
+              <Card.Title className='card-title'>{val.title}</Card.Title>
+              <Card.Text className='card-text'>{val.description}</Card.Text>
+            </Card.Body>
+          </Card>
         </a>
-      </header>
-    </div>
-  );
-}
+      </Col >
+    )
+    // 
+    //   <li className='index-ul__item' key={val.id}>
+    //     <img src={F2.s3url() + "/images/emojies/" + val.source} className='index-ul__image' />
+    //     <p className='index-ul-icons'>
+    //       <FaHeart color='#777' onClick={() => alert('Comming Soon...')} className='index-ul-icons__item cursor' />
+    //       <FaArrowDown color='#777' onClick={() => Api.download(val.source)} className='index-ul-icons__item cursor' />
+    //     </p>
+    //   </li >
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <h1 className="header-title">React News</h1>
+            <p className="header-description">Powered by NewsAPI</p>
+          </div>
+        </header>
+        <main className="main">
+          <Row>
 
+            {newsPanel}
+          </Row>
+        </main>
+        <footer className="footer">React News</footer>
+      </div>
+    );
+  }
+}
 export default App;
